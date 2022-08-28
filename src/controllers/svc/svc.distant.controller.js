a2c59d69397846019e788da1ae158ec6532a9bd6
Shiro-cha <noum.rzdr@gmail.com>
2022-08-28 04:11:40 +0300

    message

--- src/controllers/svc/svc.distant.controller.js
+++ src/controllers/svc/svc.distant.controller.js
@@ -279,7 +279,7 @@ class SvcDistant{
 							
 							if(err) throw err
 								if(err) throw err
-									let reposistoryAvaible = JSON.parse(data.toString())["local"]
+									let reposistoryAvaible = JSON.parse(data.toString())["distant"]
 									let repoIsAvaible = false
 									
 									
