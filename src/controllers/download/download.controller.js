de7909fac83ded0c134eb21ee996f5e34828fc3c
Shiro-cha <noum.rzdr@gmail.com>
2022-08-29 20:30:45 +0300

    message

--- src/controllers/download/download.controller.js
+++ src/controllers/download/download.controller.js
@@ -387,7 +387,7 @@ class Download{
 								}
 								
 								
-								if(repoIsAvaible){
+			
 									
 									let conn = new Client()
 									//execute git checkout :tags with ssh connexion
@@ -477,10 +477,7 @@ class Download{
 					password:password
 									})
 									
-								}else{
-									res.status(404)
-									res.json({message:"Container not found"})
-								}
+								
 								
 							})
 							
